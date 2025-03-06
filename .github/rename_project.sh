54a801fdc0a6f046f820a33b19da99aef02faf44
ghost1unknown <ghost1unknown@users.noreply.github.com>
2025-03-06 18:55:20 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="ghost1unknown"
+original_name="gds"
+original_urlname="gds"
+original_description="Awesome gds created by ghost1unknown"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv gds $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
